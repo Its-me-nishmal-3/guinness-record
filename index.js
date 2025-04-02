2d61ae1c13b0cc760fbdff4541bf4ea723425330
Its-me-nishmal-3 <dev.nishmal@gmail.com>
2025-04-02 04:43:13 +0000

    Auto Commit 728 - 2025-04-02T04:43:13.150Z

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ async function commitToGit() {
         // Commit with a message
         await gitCommand(`git commit -m "Auto Commit ${c} - ${new Date().toISOString()}"`);
         c++
-        
+        console.log('pushed')
 
     } catch (error) {
         console.error('Error committing to Git:', error);
